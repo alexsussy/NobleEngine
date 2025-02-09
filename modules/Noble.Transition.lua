6796bea1d1110d2f4b33c534c7ebe788641db109
Alex <alexsussman@pm.me>
2025-02-09 12:54:00 -0600

    comment out unused transitions

--- modules/Noble.Transition.lua
+++ modules/Noble.Transition.lua
@@ -201,28 +201,30 @@ function Noble.Transition:draw() end
 
 
 -- Noble Engine built-in transitions.
-import 'libraries/noble/modules/Noble.Transition/Cut.lua'
+--import 'libraries/noble/modules/Noble.Transition/Cut.lua'
 --
-import 'libraries/noble/modules/Noble.Transition/CrossDissolve.lua'
+--import 'libraries/noble/modules/Noble.Transition/CrossDissolve.lua'
 import 'libraries/noble/modules/Noble.Transition/Dip.lua'
 import 'libraries/noble/modules/Noble.Transition/DipToBlack.lua'
-import 'libraries/noble/modules/Noble.Transition/DipToWhite.lua'
+--import 'libraries/noble/modules/Noble.Transition/DipToWhite.lua'
 --
-import 'libraries/noble/modules/Noble.Transition/Imagetable.lua'
-import 'libraries/noble/modules/Noble.Transition/ImagetableMask.lua'
-import 'libraries/noble/modules/Noble.Transition/Spotlight.lua'
-import 'libraries/noble/modules/Noble.Transition/SpotlightMask.lua'
+--import 'libraries/noble/modules/Noble.Transition/Imagetable.lua'
+--import 'libraries/noble/modules/Noble.Transition/ImagetableMask.lua'
+--import 'libraries/noble/modules/Noble.Transition/Spotlight.lua'
+--import 'libraries/noble/modules/Noble.Transition/SpotlightMask.lua'
 --
-import 'libraries/noble/modules/Noble.Transition/SlideOff.lua'
-import 'libraries/noble/modules/Noble.Transition/SlideOffLeft.lua'
-import 'libraries/noble/modules/Noble.Transition/SlideOffRight.lua'
-import 'libraries/noble/modules/Noble.Transition/SlideOffUp.lua'
-import 'libraries/noble/modules/Noble.Transition/SlideOffDown.lua'
-import 'libraries/noble/modules/Noble.Transition/SlideOn.lua'
-import 'libraries/noble/modules/Noble.Transition/SlideOnLeft.lua'
-import 'libraries/noble/modules/Noble.Transition/SlideOnRight.lua'
-import 'libraries/noble/modules/Noble.Transition/SlideOnUp.lua'
-import 'libraries/noble/modules/Noble.Transition/SlideOnDown.lua'
+--import 'libraries/noble/modules/Noble.Transition/SlideOff.lua'
+--import 'libraries/noble/modules/Noble.Transition/SlideOffLeft.lua'
+--import 'libraries/noble/modules/Noble.Transition/SlideOffRight.lua'
+
+--import 'libraries/noble/modules/Noble.Transition/SlideOffUp.lua'
+--import 'libraries/noble/modules/Noble.Transition/SlideOffDown.lua'
+--import 'libraries/noble/modules/Noble.Transition/SlideOn.lua'
+--import 'libraries/noble/modules/Noble.Transition/SlideOnLeft.lua'
+--import 'libraries/noble/modules/Noble.Transition/SlideOnRight.lua'
+
+--import 'libraries/noble/modules/Noble.Transition/SlideOnUp.lua'
+--import 'libraries/noble/modules/Noble.Transition/SlideOnDown.lua'
 --
-import 'libraries/noble/modules/Noble.Transition/MetroNexus.lua'
-import 'libraries/noble/modules/Noble.Transition/WidgetSatchel.lua'
+--import 'libraries/noble/modules/Noble.Transition/MetroNexus.lua'
+--import 'libraries/noble/modules/Noble.Transition/WidgetSatchel.lua'
